dd530cb80b55e9b9fa9747a3723a178d8339ed59
SevenCats <snowcolton@hotmail.com>
2021-01-02 23:15:57 -0500

    partial emails

---
+++ email/index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs/promises'); // eslint-disable-line
+const nodemailer = require('nodemailer');
+const Handlebars = require('handlebars');
+const mjml2html = require('mjml');
+
+async function createTemplate(filePath) {
+	const mjml = await fs.readFile(path.join(__dirname, filePath), 'utf-8');
+	const template = Handlebars.compile(mjml);
+	return (...args) => mjml2html(template(...args));
+}
+
+module.exports = async function createEmail() {
+	const transport = nodemailer.createTransport({
+		host: process.env.SMTP_HOST,
+		port: process.env.SMTP_PORT,
+		secure: true,
+		pool: true,
+		auth: {
+			user: process.env.SMTP_USER,
+			pass: process.env.SMTP_PASSWORD,
+		},
+	});
+
+	async function send(subject, html) {
+		return transport.sendMail({
+			subject,
+			html,
+			from: process.env.EMAIL_FROM,
+			to: process.env.EMAIL_TO,
+		});
+	}
+
+	const templates = await Promise.all([
+		createTemplate('ban-appeal.mjml'),
+	])
+		.then(([banAppeal]) => ({ banAppeal }));
+
+	return {
+		async banAppeal(nick, templateData) {
+			return send(`Ban Appeal: ${nick}`, templates.banAppeal(templateData));
+		},
+	};
+};
--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 require('dotenv').config();
 const createLogger = require('./logger');
+const createEmail = require('./email');
 const createServer = require('./server');
 
 createServer({
 	logger: createLogger(),
+	email: createEmail(),
 });
